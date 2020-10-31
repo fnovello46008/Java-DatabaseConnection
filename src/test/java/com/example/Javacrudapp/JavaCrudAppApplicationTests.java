ea61e15fffc0f8d97b9277d90084deaddf68d3a4
fnovello46008 <fnovello46008@Gmail.com>
2020-10-31 12:45:49 -0400

    Initial commit

---
+++ src/test/java/com/example/Javacrudapp/JavaCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Javacrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
