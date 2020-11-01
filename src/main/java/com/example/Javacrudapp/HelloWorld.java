628c6890f02cdbe69bd373073f3ee172006d7dad
fnovello46008 <fnovello46008@Gmail.com>
2020-11-01 10:50:12 -0500

    Connected to Database

--- src/main/java/com/example/Javacrudapp/HelloWorld.java
+++ src/main/java/com/example/Javacrudapp/HelloWorld.java
@@ -1,14 +1,35 @@
 package com.example.Javacrudapp;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.*;
+
+
 
 @RestController
 public class HelloWorld {
 
     @RequestMapping("/")
-    public String index() {
+    public void index() {
+        String dbURL = "jdbc:mysql://localhost:8889/java";
+        String username = "fnovello";
+        String password = "novello2";
 
-        return "Hello World!";
+        try {
+
+            Connection conn = DriverManager.getConnection(dbURL, username, password);
+
+            if (conn != null) {
+                System.out.println("Connected");
+            }else {
+                System.out.println("failed");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
--- src/main/java/com/example/Javacrudapp/JavaCrudAppApplication.java
+++ src/main/java/com/example/Javacrudapp/JavaCrudAppApplication.java
@@ -3,6 +3,8 @@ package com.example.Javacrudapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class JavaCrudAppApplication {
 
